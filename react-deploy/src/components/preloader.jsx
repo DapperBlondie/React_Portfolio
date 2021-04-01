90eb09e2796c75c9733d51efd539267883b82d7f
DapperBlondie <alirezagharib110@gmail.com>
2021-04-01 23:49:03 +0430

    Update preloader.jsx

--- react-deploy/src/components/preloader.jsx
+++ react-deploy/src/components/preloader.jsx
@@ -5,7 +5,7 @@ class Preloader extends React.Component {
     componentDidMount(){
         $(window).on('load', function () {
             if ($('#preloader').length) {
-              $('#preloader').delay(50).fadeOut('slow', function () {
+              $('#preloader').delay(20).fadeOut('slow', function () {
                 $(this).remove();
               });
             }
